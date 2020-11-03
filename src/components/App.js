ed1e8d8b82ffd8f7070c52752819c6a5623d57fd
jason1794c <69557789+jason1794c@users.noreply.github.com>
2020-11-03 15:24:21 -0500

    Removed sample recipes

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState()
-  const [recipes, setRecipes] = useState(sampleRecipes)
+  const [recipes, setRecipes] = useState([])
   const selectedRecipe = recipes.find(recipe => recipe.id === selectedRecipeId)
 
   // Loading recipes from local storage
@@ -85,44 +85,4 @@ function App() {
   )
 };
 
-const sampleRecipes = [
-  {
-    id: 1,
-    name: 'Plain Chicken',
-    servings: 3,
-    cookTime: '1:45',
-    instructions: '1. Put salt on Chicken\n2. Put chicken in oven\n3. Eat chicken',
-    ingredients: [
-      {
-        id: 1,
-        name: 'Chicken',
-        amount: '2 Pounds'
-      },
-      {
-        id: 2,
-        name: 'Salt',
-        amount: '1 Tbs'
-      }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Plain Pork',
-    servings: 5,
-    cookTime: '0:45',
-    instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork',
-    ingredients: [
-      {
-        id: 1,
-        name: 'Pork',
-        amount: '3 Pounds'
-      },
-      {
-        id: 2,
-        name: 'Paprika',
-        amount: '2 Tbs'
-      }
-    ]
-  }
-]
 export default App;
